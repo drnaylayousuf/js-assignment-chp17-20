b79f8bd63391c5814c15a5b35d360704b4a30cd8
drnaylayousuf <naylayousuf.dr1310@gmail.com>
2023-09-19 04:00:53 +0500

    third assignment

---
+++ assignment.js
@@ -0,0 +1,164 @@
+// HOME ASSIGNMENT 
+// FOR LOOPS
+// CHAPTER 17-20
+
+// 1.
+
+// for (var i = 0; i< 10; i++) {
+//     console.log("hello world " + i);
+// }
+
+
+// 2.
+
+// for (var i =0; i <= 12; i++) {
+//     console.log("good morning " +  i);
+// }
+
+
+// 3.
+
+// for (var i = 0; i <= 4; i++) {
+    
+//     console.log(" Hi " + i);
+// }
+
+
+// 4.
+
+// for (var k = 0; k < 100; k++){
+//     console.log( "welcome home "+ k);
+// }
+
+// 5.
+// for (var i = 0; i > -3; i--) {
+//     console.log("aliya " + i);
+// }
+
+
+// 6.
+// var  myArray = [1,2,3,4,5,];
+// var arrayLength = myArray.length;
+
+// console.log(arrayLength);
+
+
+// 7.
+// var  flag = true;
+
+// 8.
+// for (var i = 0; i < pets.length; i++) {
+
+
+// 9.
+// for (var i = 0; i < 10; i++) {
+//     if (i === 1) {
+//        alert(i);
+//         break;
+//     }
+// }
+
+// ANOTHER WAY .....NOT 
+
+
+// for (var i = 1; i <= 10; i++) {
+//     if (i === 2) {
+//         alert("Counter value is: " + (i - 1));
+//         break;
+//     }
+// }
+
+
+// 10.
+
+// var names = [];
+
+// // Prompt the user to enter the number of names
+// var numberOfNames = prompt("Enter the number of names:");
+
+// // Loop to get the names from the user
+// for (var i = 0; i < numberOfNames; i++) {
+//   var firstName = prompt("Enter first name:");
+//   names.push(firstName);
+// }
+
+// // Check if the entered name is present in the array
+// var userName = prompt("Enter user name:");
+// if (names.includes(userName)) {
+//   alert("Enter");
+// } else {
+//   alert("Please write correct user name");
+// }
+
+//  ANOTHER WAY
+
+// var userNames = ["hawa", "bushra", "naila", "laiba"];
+
+// var firstName=prompt("Enter the first name");
+
+// for(i=0;i<userNames.length;i++){
+//     if(firstName===userNames[i]){
+//         alert("Found");
+//         break;
+//     }
+//     else{
+//         alert ("Please write correct user name");
+//         break;
+//     }
+// }
+
+
+// 11.
+
+// var matchFound = false;
+
+// for (var i = 0; i < FileList.length; i++) {
+//     if (userInput === list[i]) {
+//         alert("Match found");
+//         matchFound = true;
+//         break;
+//     }
+// }
+
+// if (! matchFound) {
+//     alert("No match found");
+// }
+
+// ANOTHER WAY
+
+// var list = ["hawa", "bushra", "naila", "laiba"];
+
+// var userInput=prompt("Enter the first name");
+
+// var matchFound = false;
+// for (var i = 0; i < list.length; i++) {
+// if (userInput === list[i]) {
+// alert("Match found");
+// matchFound = true; 
+// break;
+// }
+// else{
+//     alert ("Please write correct user name");
+//     break;
+//     }
+// }
+
+
+
+//12.
+// var firstArr = ["a", "b", "c", "d", "e", "f"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+
+// for (var i = 0; i < firstArr.length; i++) {
+//   for (var j = 0; j < secondArr.length; j++) {
+//     console.log(firstArr[i] + secondArr[j]);
+//   }
+// }
+
+// ...................end......................
+
+
+
+
+
+
